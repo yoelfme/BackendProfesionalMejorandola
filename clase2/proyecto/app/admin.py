169115f82017e1129c80fc9f78cfcb56196751a8
Yoel Monzon <yoelfme@hotmail.com>
2015-09-10 10:42:16 -0600

    iniciando clase 3

--- clase2/proyecto/app/admin.py
+++ clase2/proyecto/app/admin.py
@@ -1,12 +1,25 @@
 from django.contrib import admin
 from .models import *
+from actions import export_as_csv
 
 class CategoriAdmin(admin.ModelAdmin):
     list_display = ('titulo', )
 
 
 class EnlaceAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'enlace')
+    list_display = ('enlace', 'titulo', 'categoria', 'imagen_voto', 'es_popular')
+    list_filter = ('categoria', 'usuario')
+    search_fields = ('categoria__titulo', 'usuario__email')
+    list_editable = ('titulo', 'categoria')
+    list_display_links = ('es_popular', )
+    actions = [export_as_csv]
+
+    def imagen_voto(self, obj):
+        url = obj.mis_votos_en_imagen_rosada()
+        tag = '<img src="%s">' % url
+        return tag
+    imagen_voto.allow_tags = True
+    imagen_voto.admin_order_field = 'votos'
 
 admin.site.register(Categoria, CategoriAdmin)
 admin.site.register(Enlace, EnlaceAdmin)
--- clase2/proyecto/app/models.py
+++ clase2/proyecto/app/models.py
@@ -18,5 +18,12 @@ class Enlace(models.Model):
     def __unicode__(self):
         return "%s - %s" % (self.titulo, self.enlace)
 
+    def mis_votos_en_imagen_rosada(self):
+        return 'http://placehold.it/200x100/E8117F/ffffff/&text=%d+votos' % self.votos
+
+    def es_popular(self):
+        return self.votos > 10
+    es_popular.boolean = True
+
     class Meta:
         ordering = ['-votos']
