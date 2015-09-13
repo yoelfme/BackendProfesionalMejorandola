0961a99ae487d79a91f201ca26b4f482022159ad
Yoel Monzon <yoelfme@hotmail.com>
2015-09-13 11:32:13 -0600

    agregando middleware

---
+++ clase2/proyecto/app/middleware.py
@@ -0,0 +1,16 @@
+from django.shortcuts import redirect
+from random import choice
+
+paises = ['Colombia', 'Peru', 'Mexico']
+
+
+def de_donde_vengo(request):
+    return choice(paises)
+
+
+class PaisMiddleware():
+    def process_request(self, request):
+        pais = de_donde_vengo(request)
+
+        if pais == 'Mexico':
+            return redirect('http://platzi.com')
--- clase2/proyecto/proyecto/settings.py
+++ clase2/proyecto/proyecto/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'app.middleware.PaisMiddleware',
 )
 
 ROOT_URLCONF = 'proyecto.urls'
