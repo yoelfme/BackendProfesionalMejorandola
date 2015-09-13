e3d296ef730c285aec243f84b0731b5089f0633f
Yoel Monzon <yoelfme@hotmail.com>
2015-09-13 13:19:41 -0600

    agregando pruebas a vistas

--- clase2/proyecto/app/tests.py
+++ clase2/proyecto/app/tests.py
@@ -1,16 +1,19 @@
 from django.test import TestCase
 from .models import Categoria, Enlace
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 
 class EnlaceTest(TestCase):
+    def setUp(self):
+        self.categoria = Categoria.objects.create(titulo='Categoria de prueba')
+        self.usuario = User.objects.create_user(username='yoel', password='yoel')
+
     def test_es_popular(self):
         # si un enlace tiene menos de 11 votos no es popular
-        categoria = Categoria.objects.create(titulo='Categoria de prueba')
-        usuario = User.objects.create_user(username='yoel', password='yoel')
         enlace = Enlace.objects.create(titulo='Prueba',
                                        enlace='http://platzi.com',
-                                       categoria=categoria, usuario=usuario)
+                                       categoria=self.categoria, usuario=self.usuario)
 
         self.assertEqual(enlace.votos, 0)
         self.assertFalse(enlace.es_popular())
@@ -21,3 +24,18 @@ class EnlaceTest(TestCase):
 
         self.assertEqual(enlace.votos, 20)
         self.assertTrue(enlace.es_popular())
+
+    def test_views(self):
+        res = self.client.get(reverse('home'))
+        self.assertEqual(res.status_code, 200)
+
+        res = self.client.get(reverse('about'))
+        self.assertEqual(res.status_code, 200)
+
+        res = self.client.get(reverse('enlaces'))
+        self.assertEqual(res.status_code, 200)
+
+        self.assertTrue(self.client.login(username='yoel', password='yoel'))
+
+        res = self.client.get(reverse('add'))
+        self.assertEqual(res.status_code, 200)
