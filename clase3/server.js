43b10ed752ed029aca5b682495fefa63cff9238b
Yoel Monzon <yoelfme@hotmail.com>
2015-10-07 00:33:50 -0600

    probando rutas con parametros

--- clase3/server.js
+++ clase3/server.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var app = express();
 
+var messages = [];
+var responses = [];
+
 app.get('/', function (req, res) {
     res.send('Hello World!')
+})
+
+app.get('/messages', function (req, res) {
+    responses.push(res);
+    // res.send(messages+ '<script>setTimeout(function  () {window.location.reload()}, 1000);</script>');
+});
+
+app.get('/messages/:message', function (req, res) {
+    messages.push(req.params.message);
+
+    responses.forEach(function(res){
+        res.send(messages+ '<script>window.location.reload();</script>');
+    })
+
+    res.send('tu mensaje es ' + req.params.message);
 })
 
 var server = app.listen(3000, function(){
